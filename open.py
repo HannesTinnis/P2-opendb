9491c79a3f0c3d691160e25a7b833588e2296bb0
wrangman <wrangman@gmail.com>
2024-09-10 10:36:45 +0200

    fixed assignment

--- open.py
+++ open.py
@@ -4,7 +4,6 @@ import locale
 
 
 def load_data(filename):
-    global products 
     products = [] 
     
     with open(filename, 'r') as file:
@@ -25,23 +24,29 @@ def load_data(filename):
                     "quantity": quantity
                 }
             )
+    return products
     
 def get_products(products):
     product_list = []
+     #TODO: gör en nummerlista med enumerate (att använda id kommer inte fungera i längden)
+     
     for product in products:
-        product_info = f"{product['name']} \t {product['desc']} \t {locale.currency(product['price'], grouping=True)}"
+        product_info = f"(#{product['id']}) {product['name']} \t {product['desc']} \t {locale.currency(product['price'], grouping=True)}"
         product_list.append(product_info)
     
     return "\n".join(product_list)
 
 
-#TODO: gör om så du slipper använda global-keyword (flytta inte "product = []")
 #TODO: skriv en funktion som returnerar en specifik produkt med hjälp av id
+#TODO: skriv en funktion som tar bort en specifik produkt med hjälp av id
+
 
 
 locale.setlocale(locale.LC_ALL, 'sv_SE.UTF-8')  
 
-os.system('cls')
-load_data('db_products.csv')
+while True:
+    os.system('cls')
+    products = load_data('db_products.csv')
+    print(get_products(products))
 
-print(get_products(products))
+    id = int(input("Vilken produkt vill du visa? "))
